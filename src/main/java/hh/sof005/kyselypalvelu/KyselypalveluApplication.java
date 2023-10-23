2ec64bb9cc94054c768a03d9c228a3d66e465bef
Akseli Vartiainen <vartiainen.akseli736@gmail.com>
2023-10-23 14:15:00 +0300

    ekat projektilisäykset

---
+++ src/main/java/hh/sof005/kyselypalvelu/KyselypalveluApplication.java
@@ -0,0 +1,13 @@
+package hh.sof005.kyselypalvelu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KyselypalveluApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KyselypalveluApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hh/sof005/kyselypalvelu/KyselypalveluApplicationTests.java
@@ -0,0 +1,13 @@
+package hh.sof005.kyselypalvelu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KyselypalveluApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
