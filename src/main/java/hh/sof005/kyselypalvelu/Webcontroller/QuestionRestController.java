4cdf4758294f175eb5cb7dc6c9fbbb0b9145dbfe
Lassive <lassive9@gmail.com>
2023-10-30 12:23:12 +0200

    question rest

--- src/main/java/hh/sof005/kyselypalvelu/Webcontroller/QuestionRestController.java
+++ src/main/java/hh/sof005/kyselypalvelu/Webcontroller/QuestionRestController.java
@@ -1,5 +1,28 @@
 package hh.sof005.kyselypalvelu.Webcontroller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import hh.sof005.kyselypalvelu.Domain.Question;
+import hh.sof005.kyselypalvelu.Domain.QuestionRepository;
+
+@CrossOrigin
+@Controller
 public class QuestionRestController {
-    
+
+    @Autowired
+    private QuestionRepository questionRepository;
+
+    @RequestMapping(value="/questions", method = RequestMethod.GET)
+    public @ResponseBody List<Question> questionListRest() {
+        return (List<Question> questionRepository.findAll());
+    }
+
 }
