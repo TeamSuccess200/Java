459e6ae8ca3c11e8f35266225fab36e007b326a4
Akseli Vartiainen <vartiainen.akseli736@gmail.com>
2023-10-30 11:35:38 +0200

    added inquirylist template and method for adding inquiries

--- src/main/java/hh/sof005/kyselypalvelu/KyselypalveluApplication.java
+++ src/main/java/hh/sof005/kyselypalvelu/KyselypalveluApplication.java
@@ -1,16 +1,35 @@
 package hh.sof005.kyselypalvelu;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import hh.sof005.kyselypalvelu.Domain.Inquiry;
+import hh.sof005.kyselypalvelu.Domain.InquiryRepository;
+import hh.sof005.kyselypalvelu.Domain.QuestionRepository;
 
 @SpringBootApplication
 public class KyselypalveluApplication {
+
+	private static final Logger log = LoggerFactory.getLogger(KyselypalveluApplication.class);
 
 	public static void main(String[] args) {
 		SpringApplication.run(KyselypalveluApplication.class, args);
 
 
 		// Testikommentti
+	}
+
+	@Bean
+	public CommandLineRunner inquiryDemo(InquiryRepository irepository, QuestionRepository qrepository) {
+		return (args) -> {
+			log.info("Save some inquiries");
+			Inquiry testInquiry1 = new Inquiry("Testi", "Testikysely");
+			irepository.save(testInquiry1);
+		};
 	}
 
 }
