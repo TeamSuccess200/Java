d04e8ff10084f323773eb1f777d3820c5a7958e0
Akseli Vartiainen <vartiainen.akseli736@gmail.com>
2023-11-01 13:18:43 +0200

    added working 'addquestion' template and functionality

--- src/main/java/hh/sof005/kyselypalvelu/Webcontroller/QuestionController.java
+++ src/main/java/hh/sof005/kyselypalvelu/Webcontroller/QuestionController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import hh.sof005.kyselypalvelu.Domain.Inquiry;
 //import hh.sof005.kyselypalvelu.Domain.Inquiry;
 import hh.sof005.kyselypalvelu.Domain.InquiryRepository;
 import hh.sof005.kyselypalvelu.Domain.Question;
@@ -27,9 +28,11 @@ public class QuestionController {
     /* Lisää kysymys tiettyyn kyselyyn */
     @GetMapping("/addquestion/{inquiryId}")
     public String addQuestion(Model model, @PathVariable("inquiryId") Long inquiryId) {
-        // Luodaan Question-olio, luodaan olion Inquiry-attribuutti-olio
-        // Inquiry-attribuutti-oliolle asetetaan pyynnössä tullut inquiryId-arvo
+        Inquiry inquiry = inquiryRepository.findById(inquiryId).orElse(null);
+
         Question question = new Question();
+
+        question.setInquiry(inquiry);
 
         model.addAttribute("question", question);
         return "addQuestion";
@@ -39,16 +42,12 @@ public class QuestionController {
     @PostMapping("/savequestion")
     public String saveQuestion(@ModelAttribute("question") Question question) {
         System.out.println("Question: " + question);
-        /*
-         * Long inquiryId = question.getInquiry().getInquiryId();
-         * Optional<Inquiry> inquiryOptional = inquiryRepository.findById(inquiryId);
-         * Inquiry savedInquiry = inquiryOptional.get();
-         * question.setInquiry(savedInquiry);
-         */
 
         questionRepository.save(question);
-        return "redirect:/inquiryList";
-        // return "redirect:/inquiry/" + inquiryId;
+
+        Long inquiryId = question.getInquiry().getInquiryId();
+
+        return "redirect:/inquiry/" + inquiryId;
     }
 
     /* Näytä kysymykset */
