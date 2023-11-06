155e9df94ce1a0aa31c30fd003fca57dfcdf535f
SuviAnnina <suvi.varjoranta@gmail.com>
2023-11-06 13:01:42 +0200

    Added answer.java class

---
+++ src/main/java/hh/sof005/kyselypalvelu/Domain/Answer.java
@@ -0,0 +1,67 @@
+package hh.sof005.kyselypalvelu.Domain;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+
+@Entity
+public class Answer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long answerId;
+
+    private String answertext;
+
+    @ManyToOne
+    @JoinColumn(name = "questionid")
+    @JsonIgnoreProperties("answers")
+    private Question question;
+
+    public Answer() {
+    }
+
+    public Answer(String answertext, Question question) {
+        this.answertext = answertext;
+        this.question = question;
+    }
+
+    public Answer(String answertext) {
+        this.answertext = answertext;
+    }
+
+    public void setAnswerId(Long answerId) {
+        this.answerId = answerId;
+    }
+
+    public void setAnswertext(String answertext) {
+        this.answertext = answertext;
+    }
+
+    public void setQuestion(Question question) {
+        this.question = question;
+    }
+
+    public Long getAnswerId() {
+        return answerId;
+    }
+
+    public String getAnswertext() {
+        return answertext;
+    }
+
+    public Question getQuestion() {
+        return question;
+    }
+
+    @Override
+    public String toString() {
+        return "Answer answerId: " + answerId + ", answertext: " + answertext;
+    }
+
+}
--- src/main/java/hh/sof005/kyselypalvelu/Domain/Question.java
+++ src/main/java/hh/sof005/kyselypalvelu/Domain/Question.java
@@ -1,13 +1,17 @@
 package hh.sof005.kyselypalvelu.Domain;
 
+import java.util.List;
+
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 
 @Entity
 public class Question {
@@ -20,6 +24,10 @@ public class Question {
     @JsonIgnoreProperties("questions")
     @JoinColumn(name = "inquiryId")
     private Inquiry inquiry;
+
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "question")
+    @JsonIgnoreProperties("question")
+    private List<Answer> answers;
 
     public Question() {
     }
